ec93d88101e11f15dd09ecdcced9fbe9e4e943ba
Wendell Alves <wendellmccaulay@hotmail.com>
2021-09-15 23:17:29 -0300

    Add project

---
+++ src/test/java/com/wendell/springcloudmysql/SpringCloudMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wendell.springcloudmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
